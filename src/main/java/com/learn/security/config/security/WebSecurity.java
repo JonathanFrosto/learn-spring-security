7253550e8595bdc75fa35b8223bbd16b8e11935d
jonathan <jonataanthony@gmail.com>
2022-09-18 00:36:27 -0300

    refactor: remove deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/learn/security/config/security/WebSecurity.java
+++ src/main/java/com/learn/security/config/security/WebSecurity.java
@@ -3,46 +3,41 @@ package com.learn.security.config.security;
 import com.learn.security.config.security.filters.HeaderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurity {
 
-    private final UserDetailsService userDetailsService;
-
-    public WebSecurity(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    private HeaderFilter getHeaderFilter(AuthenticationManager authenticationManager) {
+        return new HeaderFilter(new AntPathRequestMatcher("/api/**"), authenticationManager);
     }
 
-    private HeaderFilter getHeaderFilter() throws Exception {
-        return new HeaderFilter(new AntPathRequestMatcher("/api/**"), authenticationManager());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
-                .addFilterBefore(getHeaderFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(getHeaderFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
-//                .antMatchers(HttpMethod.PATCH, "/api/users/{id}/admin").hasRole("ADMIN")
                 .and()
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .csrf().disable();
+
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
